a6fc6713aa71b8c25b3745f497840fbc90a82d93
Javar <javar.alexander@gmail.com>
2021-08-11 18:16:48 -0400

    Code cleanup

--- pages/[...link].js
+++ pages/[...link].js
@@ -1,55 +1,53 @@
-import { connectToDatabase } from "../lib/mongodb"
+import { connectToDatabase } from '../lib/mongodb'
 
-import { useEffect } from "react"
+import { useEffect } from 'react'
 
 export async function getServerSideProps(context) {
-    
-    const {link: myLink} = context.query
+    const { link: myLink } = context.query
     const { client, db } = await connectToDatabase()
     const collection = db.collection('shortner')
     const isConnected = await client.isConnected()
 
-    
-
     if (isConnected) {
-
         const formattedLink = `${process.env.DOMAIN}/${myLink}`
+        const { originalURL } = getUrlFromShortCode(collection)
 
-        const options = {
-            // Include only the `title` and `imdb` fields in the returned document
-            projection: { originalURL: 1, generatedLink: 1 },
-          };
-
-      
-
-        const foundThing =   await collection.find({generatedLink : formattedLink}, options).toArray()
-
-        return {
-            props: {
-                generatedLink:formattedLink,
-                destinationURL: foundThing[0]?.originalURL
-            }, // will be passed to the page component as props
-          } 
+        return originalURL
+            ? {
+                  props: {
+                      generatedLink: formattedLink,
+                      destinationURL: originalURL,
+                  },
+              }
+            : redirectToIndex()
     } else {
-        // return {
-        //     redirect: {
-        //         permanent: true,
-        //         destination: '/',
-        //       },
-        // }
+        return redirectToIndex()
+    }
+}
+
+const getUrlFromShortCode = async (collection) => {
+    const queryOptions = {
+        projection: { originalURL: 1, generatedLink: 1 },
     }
 
-  
-   
-  }
+    const foundRecord = await collection
+        .find({ generatedLink: formattedLink }, queryOptions)
+        .toArray()
+    return foundRecord.length ? foundRecord[0] : []
+}
 
+const redirectToIndex = () => ({
+    redirect: {
+        permanent: true,
+        destination: '/',
+    },
+})
 
-const Redirect = ({destinationURL}) => {
+const Redirect = ({ destinationURL }) => {
     useEffect(() => {
         window.location.replace(destinationURL)
-    },[])
-return <></>
-
+    }, [])
+    return <></>
 }
 
 export default Redirect
--- pages/api/url-shortner.js
+++ pages/api/url-shortner.js
@@ -1,44 +1,23 @@
 import { connectToDatabase } from '../../lib/mongodb'
-import { csrf } from '../../lib/csrf';
-import { generateLink } from '../../lib/linkGenerator';
+import { csrf } from '../../lib/csrf'
+import { insertNewRecord } from '../../lib/util'
 
-
-export const handler = async(req, res) => {
+const handler = async (req, res) => {
     if (req.method == 'POST') {
         const { client, db } = await connectToDatabase()
         const collection = db.collection('shortner')
         const isConnected = await client.isConnected()
-        const {url} = req.body
-
-        
-        const insertNewRecord = async () => {
-                try {
-                    const record = {
-                        generatedLink: generateLink(),
-                        originalURL: url
-                    }
-                    await collection.insertOne(record)
-                    return record
-                } catch (e) {
-                    res.status(500).json({error: 'Internal Server Error'})
-                }
-        }
-    
-
+        const { url } = req.body
 
         if (isConnected) {
-           const data = await insertNewRecord()
-            res.status(200).json(data)
-        } else {
-            res.status(500).json({error: 'Internal Server Error'})
+            const record = await insertNewRecord(collection, url)
+            return record
+                ? res.status(200).json(record)
+                : res.status(500).json({ error: 'Internal Server Error' })
         }
-    } else {
-        res.status(404)
-        res.send('Not found')
     }
-  }
+    res.status(404)
+    res.send('Not found')
+}
 
-  export default handler //csrf(handler);
- 
-
-
+export default handler //csrf(handler)
