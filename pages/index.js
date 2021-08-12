20ca9f4fc19712ac7a7a473d09be92aa43dcefdc
Javar <javar.alexander@gmail.com>
2021-08-12 13:05:45 -0400

    added basic stlying & react spinner loader

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,25 @@
 import Head from 'next/head'
 import { useState } from 'react'
-
 import { csrfToken } from '../lib/csrf'
-import { createShortLinkRequest } from '../lib/util'
+import { createShortLink } from '../lib/util'
+import Loader from 'react-loader-spinner'
 
 const Home = () => {
     const [link, setLink] = useState('')
     const [shortLink, setShortLink] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
 
     const makeRequest = async () => {
         if (link) {
+            setIsLoading(true)
             try {
-                const { data } = await createShortLinkRequest(csrfToken, link)
+                const { data } = await createShortLink(csrfToken, link)
                 setShortLink(data)
+                setIsLoading(false)
+                setLink('')
             } catch (e) {
                 console.warn(e)
+                setIsLoading(false)
             }
         } else {
             alert('Invalid URL')
@@ -28,7 +33,7 @@ const Home = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <div class="body">
+            <div class="wrapper">
                 <h1 className="title">Next Shortner</h1>
 
                 <h2 className="subtitle">
@@ -39,10 +44,24 @@ const Home = () => {
                     value={link}
                     placeholder="https://example.com/felknjfeelknfelkn;gfnklefg/"
                     type="text"
+                    className={`url--input`}
                     onChange={(e) => setLink(e.target.value)}
                 />
-                <button onClick={makeRequest}> Go!</button>
-                {shortLink && (
+                <button className={`btn--submit`} onClick={makeRequest}>
+                    {' '}
+                    Go!
+                </button>
+                {isLoading && (
+                    <div className="loader">
+                        <Loader
+                            type="TailSpin"
+                            color="#2b2d2f"
+                            height={75}
+                            width={50}
+                        />
+                    </div>
+                )}
+                {shortLink && !isLoading && (
                     <div className="grid">
                         <a href={`${shortLink.generatedLink}`} className="card">
                             {shortLink.generatedLink}
@@ -50,157 +69,6 @@ const Home = () => {
                     </div>
                 )}
             </div>
-
-            <style jsx>{`
-                .container {
-                    min-height: 100vh;
-                    padding: 0 0.5rem;
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                    align-items: center;
-                }
-
-                .body {
-                    padding: 5rem 0;
-                    flex: 1;
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                    align-items: center;
-                }
-
-                footer {
-                    width: 100%;
-                    height: 100px;
-                    border-top: 1px solid #eaeaea;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                }
-
-                footer img {
-                    margin-left: 0.5rem;
-                }
-
-                footer a {
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                }
-
-                a {
-                    color: inherit;
-                    text-decoration: none;
-                }
-
-                .title a {
-                    color: #0070f3;
-                    text-decoration: none;
-                }
-
-                .title a:hover,
-                .title a:focus,
-                .title a:active {
-                    text-decoration: underline;
-                }
-
-                .title {
-                    margin: 0;
-                    line-height: 1.15;
-                    font-size: 4rem;
-                }
-
-                .title,
-                .description {
-                    text-align: center;
-                }
-
-                .subtitle {
-                    font-size: 2rem;
-                }
-
-                .description {
-                    line-height: 1.5;
-                    font-size: 1.5rem;
-                }
-
-                code {
-                    background: #fafafa;
-                    border-radius: 5px;
-                    padding: 0.75rem;
-                    font-size: 1.1rem;
-                    font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-                        DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New,
-                        monospace;
-                }
-
-                .grid {
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    flex-wrap: wrap;
-
-                    max-width: 800px;
-                    margin-top: 3rem;
-                }
-
-                .card {
-                    margin: 1rem;
-                    flex-basis: 45%;
-                    padding: 1.5rem;
-                    text-align: left;
-                    color: inherit;
-                    text-decoration: none;
-                    border: 1px solid #eaeaea;
-                    border-radius: 10px;
-                    transition: color 0.15s ease, border-color 0.15s ease;
-                }
-
-                .card:hover,
-                .card:focus,
-                .card:active {
-                    color: #0070f3;
-                    border-color: #0070f3;
-                }
-
-                .card h3 {
-                    margin: 0 0 1rem 0;
-                    font-size: 1.5rem;
-                }
-
-                .card p {
-                    margin: 0;
-                    font-size: 1.25rem;
-                    line-height: 1.5;
-                }
-
-                .logo {
-                    height: 1em;
-                }
-
-                @media (max-width: 600px) {
-                    .grid {
-                        width: 100%;
-                        flex-direction: column;
-                    }
-                }
-            `}</style>
-
-            <style jsx global>{`
-                html,
-                body {
-                    padding: 0;
-                    margin: 0;
-                    font-family: -apple-system, BlinkMacSystemFont, Segoe UI,
-                        Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
-                        Helvetica Neue, sans-serif;
-                }
-
-                * {
-                    box-sizing: border-box;
-                }
-            `}</style>
         </div>
     )
 }
